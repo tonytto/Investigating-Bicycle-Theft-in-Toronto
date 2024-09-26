9acb586fcf5af80ec9352f75bc1b5fc8d0890e3c
tonytto <taejunjumm@gmail.com>
2024-09-26 01:25:44 -0400

    Improved appearances of graphs

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -16,7 +16,7 @@ raw_data <- read_csv("data/raw_data/raw_data.csv")
 
 cleaned_data <-
   raw_data|>
-  select("_id", OCC_DATE, OCC_YEAR, PREMISES_TYPE, BIKE_COST)|>
+  select("_id", OCC_DATE, OCC_YEAR, OCC_MONTH, OCC_HOUR, PREMISES_TYPE, BIKE_COST)|>
   janitor::clean_names()
 
 cleaned_data <-
@@ -39,19 +39,15 @@ cleaned_data<-
   cleaned_data|>
   filter(occ_year > 2013, occ_year < 2024)|> 
   tidyr::drop_na()
-
-cleaned_data <-
-  cleaned_data|>mutate(
-  cost_group =
-      factor(cost_group, levels = c("< $500", "$500 < $1000", "$1000 < $1500", "$1500 < $2000", "$2000 <")))
-
-cleaned_data |>
-  ggplot(mapping = aes(x=occ_year, fill = cost_group)) + 
-  geom_bar(position = "dodge2") +
-  labs(x = "Year", y = "Cost of Bicycle", fill = "Cost Group") +
-  theme(legend.position = "bottom")
-
-
+# 
+# cleaned_data <-
+#   cleaned_data|>mutate(
+#   cost_group =
+#       factor(cost_group, levels = c("< $500", "$500 < $1000", "$1000 < $1500", "$1500 < $2000", "$2000 <")))
+# 
+# cleaned_data$occ_month <- 
+#   factor(cleaned_data$occ_month, levels = c("January", "February", "March", "April", "May", "June", 
+#                                         "July", "August", "September", "October", "November", "December"))
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
